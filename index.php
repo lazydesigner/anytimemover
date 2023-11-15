7c2460128fae498b4ccdac2756baa46184933892
deepak <deepakbaradwaj933@gmail.com>
2023-11-15 12:28:44 +0530

    improvement

--- index.php
+++ index.php
@@ -45,7 +45,7 @@
         </video>
         <header>
             <nav>
-                <a href="./" class="flex1"><img src="./assets/images/Logo2.png" width="100%" alt="" style="object-fit:cover;"></a>
+                <a href="<?=base_urlx() ?>" class="flex1"><img src="./assets/images/Logo2.png" width="100%" height="100%" alt="" style="object-fit:cover;"></a>
                 <div id="three-line-menu" class="three-line-menu"><i class="ri-menu-3-line"></i></div>
                 <ul class="flex2" id="flex2">
                     <div class="close-the-nav" id="close-the-nav">X</div>
@@ -61,8 +61,8 @@
                 <h1>DEDICATED . EFFICIENT . RELIABLE </h1>
                 <p>Anytime Movers is built on providing the most value to our customers. We want to be the most dedicated, efficient, and reliable moving company you have ever worked with.</p>
                 <div class="hero-section-button">
-                    <a href=""><button class="btn">People are Talking</button></a>
-                    <a href=""><button class="btn2">Get A FREE Quote</button></a>
+                    <a href="<?=base_urlx() ?>"><button class="btn">People are Talking</button></a>
+                    <a href="<?=base_urlx() ?>"><button class="btn2">Get A FREE Quote</button></a>
                 </div>
 
             </div>
@@ -96,31 +96,31 @@
                 <div class="list-of-service-img"><img src="<?=base_urlx() ?>assets/images/South-Florida-Movers1-e1463070625660.png" alt=""></div>
                 <h3>RESIDENTIAL MOVES</h3>
                 <p>We understand changing homes can be stressful, but your moving experience doesn’t have to be.</p>
-                <a href="services-page" style="cursor: pointer;"><button><i class="ri-truck-fill"></i>Learn More</button></a>
+                <a href="<?=base_urlx() ?>services-page" style="cursor: pointer;"><button><i class="ri-truck-fill"></i>Learn More</button></a>
             </div>
             <div class="box">
                 <div class="list-of-service-img"><img src="<?=base_urlx() ?>assets/images/Tri-country-movers.png" alt=""></div>
                 <h3>CORPORATE RELOCATION</h3>
                 <p>When it come to commercial moves, no job is too big or too small. </p>
-                <a href="services-page" style="cursor: pointer;"><button><i class="ri-truck-fill"></i>Learn More</button></a>
+                <a href="<?=base_urlx() ?>services-page" style="cursor: pointer;"><button><i class="ri-truck-fill"></i>Learn More</button></a>
             </div>
             <div class="box">
                 <div class="list-of-service-img"><img src="<?=base_urlx() ?>assets/images/Moving-South-Florida.png" alt=""></div>
                 <h3>PRICING OPTIONS</h3>
                 <p>We try to accommodate every customer on every job. See our pricing options.</p>
-                <a href="services-page" style="cursor: pointer;"><button><i class="ri-money-dollar-circle-line"></i>Learn More</button></a>
+                <a href="<?=base_urlx() ?>services-page" style="cursor: pointer;"><button><i class="ri-money-dollar-circle-line"></i>Learn More</button></a>
             </div>
             <div class="box">
                 <div class="list-of-service-img"><img src="<?=base_urlx() ?>assets/images/Best-Movers-South-Florida.png" alt=""></div>
                 <h3>LONG DISTANCE MOVES</h3>
                 <p>We handle long distance moves with the same dedication and care as our local moves. </p>
-                <a href="services-page" style="cursor: pointer;"><button><i class="ri-shuffle-line"></i>Learn More</button></a>
+                <a href="<?=base_urlx() ?>services-page" style="cursor: pointer;"><button><i class="ri-shuffle-line"></i>Learn More</button></a>
             </div>
             <div class="box">
                 <div class="list-of-service-img"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M22 13.5V21C22 21.5523 21.5523 22 21 22H20C19.4477 22 19 21.5523 19 21V20H5V21C5 21.5523 4.55228 22 4 22H3C2.44772 22 2 21.5523 2 21V13.5L0.757464 13.1894C0.312297 13.0781 0 12.6781 0 12.2192V11.5C0 11.2239 0.223858 11 0.5 11H2.375L4.51334 5.29775C4.80607 4.51715 5.55231 4 6.386 4H17.614C18.4477 4 19.1939 4.51715 19.4867 5.29775L21.625 11H23.5C23.7761 11 24 11.2239 24 11.5V12.2192C24 12.6781 23.6877 13.0781 23.2425 13.1894L22 13.5ZM4 15V17C4 17.5523 4.44772 18 5 18H8.24496C8.3272 18 8.40818 17.9797 8.4807 17.9409C8.72418 17.8107 8.81602 17.5078 8.68582 17.2643L8.68588 17.2643C7.87868 15.7548 6.31672 15 4 15ZM20 15C17.6833 15 16.1213 15.7548 15.3141 17.2643L15.3142 17.2643C15.184 17.5078 15.2758 17.8107 15.5193 17.9409C15.5918 17.9797 15.6728 18 15.755 18H19C19.5523 18 20 17.5523 20 17V15ZM6 6L4.43874 10.6838C4.40475 10.7857 4.38743 10.8925 4.38743 11C4.38743 11.5523 4.83514 12 5.38743 12H18.6126C18.7201 12 18.8268 11.9827 18.9288 11.9487C19.4527 11.774 19.7359 11.2077 19.5613 10.6838L18 6H6Z"></path></svg></div>
                 <h3>CAR TRANSPORT</h3>
                 <p>We Also Ship Car When Your Are Shifting</p>
-                <a href="more-services" style="cursor: pointer;"><button><i class="ri-shuffle-line"></i>Learn More</button></a>
+                <a href="<?=base_urlx() ?>more-services" style="cursor: pointer;"><button><i class="ri-shuffle-line"></i>Learn More</button></a>
             </div>
         </div>
     </section>
@@ -189,7 +189,7 @@
                     <input type="text" placeholder="Email*">
                     <input type="text" placeholder="Subject*">
                     <textarea name="" id="" cols="30" rows="7" placeholder="Your Message"></textarea>
-                    <div><input type="checkbox" name="" id=""><span style="color:lightblue;">I agree with this Site's Privacy Policy</span></div>
+                    <div><input type="checkbox" name="terms" id="terms"><span style="color:lightblue;">I agree with this Site's Privacy Policy</span></div>
                     <input type="submit" value="Send">
                 </form>
             </div>
@@ -208,7 +208,7 @@
                 <div class="blogs">
                     <div class="blogs-img"><img src="<?=base_urlx() ?>assets/images/open Air Transport.webp" alt=""></div>
                     <div style="padding:1% 2%;">
-                    <a href="#"><h4>Lorem ipsum dolor sit amet.</h4></a>
+                    <a href="#"><h3>Lorem ipsum dolor sit amet.</h3></a>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolor fugit inventore sunt nemo quae voluptas ex fuga veritatis necessitatibus!</p>
                     </div>
                 </div>
@@ -217,7 +217,7 @@
                 <div class="blogs">
                     <div class="blogs-img"><img src="<?=base_urlx() ?>assets/images/17899070.webp" alt=""></div>
                     <div style="padding:1% 2%;">
-                    <a href="#"><h4>Lorem ipsum dolor sit amet.</h4></a>
+                    <a href="#"><h3>Lorem ipsum dolor sit amet.</h3></a>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolor fugit inventore sunt nemo quae voluptas ex fuga veritatis necessitatibus!</p>
                     </div>
                 </div>
@@ -226,7 +226,7 @@
                 <div class="blogs">
                     <div class="blogs-img"><img src="<?=base_urlx() ?>assets/images/87210796.webp" alt=""></div>
                     <div style="padding:1% 2%;">
-                    <a href="#"><h4>Lorem ipsum dolor sit amet.</h4></a>
+                    <a href="#"><h3>Lorem ipsum dolor sit amet.</h3></a>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non dolor fugit inventore sunt nemo quae voluptas ex fuga veritatis necessitatibus!</p>
                     </div>
                 </div>
