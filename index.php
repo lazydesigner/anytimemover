4a398ddfed60bdf2058b6590de23fc69b6fd41ae
deepak <deepakbaradwaj933@gmail.com>
2023-11-15 12:30:20 +0530

    improvement

--- index.php
+++ index.php
@@ -31,7 +31,7 @@
     <meta property="article:tag" content="car movers near me" />
     <meta property="article:tag" content="auto moving companies" />
 
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/remixicon/3.5.0/remixicon.min.css" integrity="sha512-/VYneElp5u4puMaIp/4ibGxlTd2MV3kuUIroR3NSQjS2h9XKQNebRQiyyoQKeiGE9mRdjSCIZf9pb7AVJ8DhCg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preload" href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" as="style" onload="this.onload=null;this.rel='stylesheet'" async>
     <link rel="stylesheet" href="<?=base_urlx() ?>assets/css/index.css">
     <link rel="stylesheet" href="<?=base_urlx() ?>assets/css/footer.css">
     <link rel="stylesheet" href="<?=base_urlx() ?>assets/css/navbar.css">
