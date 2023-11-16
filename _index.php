08cf1287b04767aab4a5cab0448c929f16d503cb
deepak <deepakbaradwaj933@gmail.com>
2023-11-16 12:36:13 +0530

    improvement

--- _index.php
+++ _index.php
@@ -87,7 +87,7 @@ if(mysqli_num_rows($result) > 0){
         }
         .multiline-ellipsis {
             overflow: hidden;
-            /* display: -webkit-box; */
+            display: -webkit-box;
             -webkit-box-orient: vertical;
             -webkit-line-clamp: 4; /* start showing ellipsis when 3rd line is reached */
             white-space: pre-wrap; /* let the text wrap preserving spaces */
