a73886c0dc7d126d07269cfdb7a82be0a8bf7989
deepak <deepakbaradwaj933@gmail.com>
2023-11-18 14:06:24 +0530

    commit

--- _index.php
+++ _index.php
@@ -180,12 +180,12 @@ if(mysqli_num_rows($result) > 0){
                                     <div class="box-review-profile">
                                         <div class="box-profile"><img src="./assets/images/profile-2.jpeg" width="100%" height="100%" alt=""></div>
                                         <span>
-                                            <h3>Deepak </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
+                                            <h3>Mark Hill </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
                                         </span>
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/google.png" style="object-fit: contain;" width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">Their auto shipping services are accurate whether it is home pickup or taking care of my vehicles. I was happy with all their services and their shipping and transport services were better than anyone else.</p>
                             </div>
                         </div>
                     </div>
@@ -196,12 +196,12 @@ if(mysqli_num_rows($result) > 0){
                                     <div class="box-review-profile">
                                         <div class="box-profile"><img src="./assets/images/profile-2.jpeg" width="100%" height="100%" alt=""></div>
                                         <span>
-                                            <h3>Deepak </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
+                                            <h3>John Logan </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
                                         </span>
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/transport-reviews.png" style="object-fit: contain;" width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">I asked them for car shipping in a trailer to Vegas. I have been using their services for many years and I am never disappointed. The vehicles arrived quickly and safely.</p>
                             </div>
                         </div>
                     </div>
@@ -212,12 +212,12 @@ if(mysqli_num_rows($result) > 0){
                                     <div class="box-review-profile">
                                         <div class="box-profile"><img src="./assets/images/profile-2.jpeg" width="100%" height="100%" alt=""></div>
                                         <span>
-                                            <h3>Deepak </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
+                                            <h3>Angela Merkle </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
                                         </span>
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/google.png" style="object-fit: contain;" width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">Very accurate and in-time services. Their home pickup was excellent, and auto shippers took care of the vehicle very well. If I had known they were so good, I would have also asked them for car shipping.</p>
                             </div>
                         </div>
                     </div>
@@ -228,12 +228,12 @@ if(mysqli_num_rows($result) > 0){
                                     <div class="box-review-profile">
                                         <div class="box-profile"><img src="./assets/images/profile-2.jpeg" width="100%" height="100%" alt=""></div>
                                         <span>
-                                            <h3>Deepak </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
+                                            <h3>Barbara Allison </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
                                         </span>
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/trustpilot.png" style="object-fit: contain;" width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">I couldn't be happier with the service I received from this car transportation company when I engaged them to move my vintage vehicle. My automobile was handled carefully, and it arrived in the same state as when it left. Throughout the entire process, there was excellent communication, and I valued the assurance that my car was in capable hands. I got all my 4 vehicle moved from them the best ever experience with the car transportation company. Thank You AnyTime Movers. 5 Stars!</p>
                             </div>
                         </div>
                     </div>
@@ -244,12 +244,12 @@ if(mysqli_num_rows($result) > 0){
                                     <div class="box-review-profile">
                                         <div class="box-profile"><img src="./assets/images/profile-2.jpeg" width="100%" height="100%" alt=""></div>
                                         <span>
-                                            <h3>Deepak </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
+                                            <h3>Beth Sever </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
                                         </span>
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/bbb.png" style="object-fit: contain;" width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">Just book your next car shipment with them they are quick and reliable company to work with as they do not change their prices at the last moment which other car shipping company always does.</p>
                             </div>
                         </div>
                     </div>
@@ -260,12 +260,12 @@ if(mysqli_num_rows($result) > 0){
                                     <div class="box-review-profile">
                                         <div class="box-profile"><img src="./assets/images/profile-1.jpeg" width="100%" height="100%" alt=""></div>
                                         <span>
-                                            <h3>Deepak </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
+                                            <h3>Jerry Merten </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
                                         </span>
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/transport-reviews.png" style="object-fit: contain;" width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">Best Car Transport company out of all the car shipping company. Shipping a car from one state to the other state with Anytime Mover is the best experience of car shipping i ever had as they were quick and easy to respond during the whole shipment process.</p>
                             </div>
                         </div>
                     </div>
@@ -281,7 +281,7 @@ if(mysqli_num_rows($result) > 0){
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/trustpilot.png" style="object-fit: contain;" width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">Best Auto transport company out of all the car shipping companies in my knowledge. They have transported my car to California and once to Texas and i am the satisfied customer of Anytime Mover. The time my vehicle was getting Shipped by Anytime Mover</p>
                             </div>
                         </div>
                     </div>
@@ -292,12 +292,12 @@ if(mysqli_num_rows($result) > 0){
                                     <div class="box-review-profile">
                                         <div class="box-profile"><img src="./assets/images/profile-1.jpeg" width="100%" height="100%" alt=""></div>
                                         <span>
-                                            <h3>Deepak </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
+                                            <h3>Kevin Carree </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
                                         </span>
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/google.png" style="object-fit: contain;" width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">Shipping A vehicle from Texas to California was the best experience with Anytime Mover. Auto Transportation company usually charge high at the last minute whereas the timing and no increase in price was the best i can expect from any Anytime Mover.</p>
                             </div>
                         </div>
                     </div>
@@ -308,12 +308,12 @@ if(mysqli_num_rows($result) > 0){
                                     <div class="box-review-profile">
                                         <div class="box-profile"><img src="./assets/images/profile-1.jpeg" width="100%" height="100%" alt=""></div>
                                         <span>
-                                            <h3>Deepak </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
+                                            <h3>JandE Essam </h3><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i><i class="ri-star-s-fill"></i>
                                         </span>
                                     </div>
                                     <div class="box-profile" style="background-color: white;"><img src="./assets/images/bbb.png" style="object-fit: contain;"  width="100%" height="100%" alt=""></div>
                                 </div>
-                                <p style="text-align: start;font-size:15px;line-height:23px;">With this setup, the flex items will adjust their size and layout based on the available space, creating a responsive design that works well on different screen sizes. You can modify the styles and structure according to your specific layout requirements.</p>
+                                <p style="text-align: start;font-size:15px;line-height:23px;">Best vehicle shipping company. I have used them quiet a few times and they were proved to be the best one one as always.  Anytime Mover is one of the best car transportation in state to state shipping for USA</p>
                             </div>
                         </div>
                     </div>
