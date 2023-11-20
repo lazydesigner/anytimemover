d5a2fd2112577078086b2c4575220528e75cd085
deepak <deepakbaradwaj933@gmail.com>
2023-11-20 14:01:06 +0530

    a

---
+++ calculator.php
@@ -0,0 +1,17 @@
+<?php include './routes.php' ?>
+<!doctype html>
+<html lang=en>
+<head>
+<meta charset=UTF-8>
+<meta name=viewport content="width=device-width,initial-scale=1">
+<title>Caculotor</title>
+<link rel=preload href=https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css as=style onload='this.onload=null,this.rel="stylesheet"' async>
+<link rel=stylesheet href="<?=base_urlx() ?>assets/css/index.css" async>
+<link rel=stylesheet href="<?=base_urlx() ?>assets/css/footer.css" async>
+<link rel=stylesheet href="<?=base_urlx() ?>assets/css/navbar.css" async>
+</head>
+<body>
+<?php include './navbar.php'; ?>
+<?php include './footer.php' ?>
+</body>
+</html>
