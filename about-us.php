93f664918cdff13aaa6e1cd494c6a33773c1eae5
deepak <deepakbaradwaj933@gmail.com>
2023-11-24 17:32:22 +0530

    a

--- about-us.php
+++ about-us.php
@@ -1,53 +1,122 @@
 <?php include './routes.php' ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>About Us - Anytime Movers</title>
     <meta name="google-site-verification" content="hD9r2-2bnGslcHbfsdFhf3xbpYGfWY0Mhr-8WERfEF4" />
-<link rel=preload href=https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css as=style onload='this.onload=null,this.rel="stylesheet"' async>
-    <link rel="stylesheet" href="<?=base_urlx() ?>assets/css/footer.css">
-    <link rel="stylesheet" href="<?=base_urlx() ?>assets/css/navbar.css">
-    <link rel="stylesheet" href="<?=base_urlx() ?>assets/css/about.css?v=1">
+    <link rel=preload href=https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css as=style onload='this.onload=null,this.rel="stylesheet"' async>
+    <link rel="stylesheet" href="<?= base_urlx() ?>assets/css/footer.css">
+    <link rel="stylesheet" href="<?= base_urlx() ?>assets/css/navbar.css">
+    <link rel="stylesheet" href="<?= base_urlx() ?>assets/css/about.css?v=2">
 </head>
+
 <body>
-    <div  style="position:relative; width:100%;height:70px"><?php include './navbar.php'; ?></div>
+    <div style="position:relative; width:100%;height:70px"><?php include './navbar.php'; ?></div>
     <section class="about-us" style="text-align: center;">
         <!-- <p class="about-p">What to expect when</p> -->
-        <h1 class="about-h">About us page</h1>
+        <h1 class="about-h">About Us</h1>
         <div class="design">
-                <div class="line1"></div>
-                <div class=""><i class="ri-star-fill"></i></div>
-                <div class="line1"></div>
-            </div>
+            <div class="line1"></div>
+            <div class=""><i class="ri-star-fill"></i></div>
+            <div class="line1"></div>
+        </div>
     </section>
     <section class="para">
-     
-<p>Welcome to Anytime Movers, USA's trusted experts in moving that can help you relocate your belongings to and from any part of the USA. Our experience made us take pride in being seamless and reliable movers for many families and business firms as they allowed us to ship their belongings, office requirements and cars. We have satisfied many customers with our service and custom approach to our clients.</p>
 
-<h2>About Us:</h2>
-<p>Here at Anytime Movers, we understand that moving could be challenging and time-consuming as relocation is not a regular thing it comes with its unique set of situations and responsibilities. We have employed experts dedicated to this field, We are here to help you in any situation related to moving by making moving an easy and stress-free process so that you can shift stress-free and we can bring your belongings to your new place.</p>
-<h2>Who We Are:</h2>
-<p>Anytime Movers is a privately owned company that is located in Florida. We have employed experts who are skilled professionals in residential moves, corporate relocations, car shipping, and long-distance moves. Our team bring dedication, expertise and proper supervision to your moving requirements.</p>
-<h2>What Sets Us Apart:</h2>
-<p>Personalized  Approach: We understand that every customer is unique, as well as their moving requirements. We try to provide you with services according to your needs so that you can best out of us and get complete and satisfying moving service.</p>
-<p><strong>Expert at Work:</strong> Our team is well-trained and experienced contributing best to the table. Our team is committed to their work they take every task from packing and loading, very sincerely, which makes us deliver the highest standard of services with care and precision.</p>
-<p><strong>Inclusive Services:</strong> Whether shifting to a new home, relocating your whole office or wanting to ship your car under a long-distance move. We are here with expertise and resources to make your moving process smooth and reliable.</p>
-<p><strong>Transparent pricing:</strong> We believe in transparency whether it is a working quote or working, we provide the best. From quote to your home we try to optimize the resources with proper and clear communication and genuine pricing.</p>
-<p><strong>Advanced Equipment:</strong> Like our experts, we have the latest technology whether it is packing machinery or a fleet of carriers, we are equipped with the best to ensure that your belonging is shipped with safety and security.</p>
-
-<h2>Why Choose Us</h2>
-<p><strong>Reliability:</strong> Our experience and working made us a reliable company in the Moving industry. Many customers have already trusted us with our most valuable and sensitive possessions.</p>
-<p><strong>Experience:</strong> With years of working in this industry, we have discovered and perfected almost every skill that is important for moving your belongings. </p>
-<p><strong>Custom Approach:</strong> We understand everyone is different along with their needs. Our experts are here to help and provide you with the best solutions that fit your requirements well.</p>
-<p><strong>Affordability:</strong> We do not compromise the level of service with cost. We try to provide you best service with transparent and competitive pricing.</p>
-<h2>Contact Us</h2>
-<p>Are you ready to make your moving easy and fast . Contact Anytime movers experts by mailing or contacting at 984-235-6077 for free but accurate consultancy by our expert. Go to our Cost calculator section to discover quotes about your requirements. </p><p>
-With Anytime Movers you will get stress-free, efficient and reliable moving of your belongings.</p>
-
+        <div class="about-contact-row2" style="align-items:center">
+            <div class="main-p">
+                <div class="main-p-img"><img src="./assets/images/polotno2.png" width="100%" height="100%" alt=""></div>
+            </div>
+            <div class="main-p">
+            <p>Welcome to Anytime Movers, USA's trusted experts in moving that can help you relocate your belongings to and from any part of the USA. Our experience made us take pride in being seamless and reliable movers for many families and business firms as they allowed us to ship their belongings, office requirements and cars. We have satisfied many customers with our service and custom approach to our clients.</p>
+            <p>Here at Anytime Movers, we understand that moving could be challenging and time-consuming as relocation is not a regular thing it comes with its unique set of situations and responsibilities. We have employed experts dedicated to this field, We are here to help you in any situation related to moving by making moving an easy and stress-free process so that you can shift stress-free and we can bring your belongings to your new place.</p>
+                </div>
+        </div>
     </section>
+    <div class="about-contact-row2">
+        <div class="who-we-are-col">
+            <div class="who-we-are">
+            <h2>Who We Are</h2>
+            <p>Anytime Movers is a privately owned company that is located in Florida. We have employed experts who are skilled professionals in residential moves, corporate relocations, car shipping, and long-distance moves. Our team bring dedication, expertise and proper supervision to your moving requirements.</p>
+            </div>
+        </div>
+        <div class="who-we-are-col">
+            <div class="who-we-are-img">
+                <img src="./assets/images/polotno.png" width="100%" height="100%" alt="">
+            </div>
+        </div>
+    </div>
+
+    <div class="about-apart">
+        <div class="about-apart-row">
+            <div class="about-apart-col">
+                <div class="about-apart-col-detail">
+                <h2 style="text-align: center;">What Sets Us Apart</h2>
+                </div>
+            </div>
+            <div class="about-apart-col">
+                <div class="about-apart-grid">
+                    <div class="about-apart-grid-col">
+                        <h3>Personalized Approach</h3>
+                        <p> We understand that every customer is unique, as well as their moving requirements. We try to provide you with services according to your needs so that you can best out of us and get complete and satisfying moving service.</p>
+                    </div>
+                    <div class="about-apart-grid-col">
+                    <h3>Expert at Work</h3>
+                        <p>Our team is well-trained and experienced contributing best to the table. Our team is committed to their work they take every task from packing and loading, very sincerely, which makes us deliver the highest standard of services with care and precision.</p>
+                    </div>
+                    <div class="about-apart-grid-col">
+                     <h3>Inclusive Services</h3>
+                        <p>Whether shifting to a new home, relocating your whole office or wanting to ship your car under a long-distance move. We are here with expertise and resources to make your moving process smooth and reliable.</p>
+                    </div>
+                    <div class="about-apart-grid-col">
+                    <h3>Transparent pricing</h3>
+                        <p>We believe in transparency whether it is a working quote or working, we provide the best. From quote to your home we try to optimize the resources with proper and clear communication and genuine pricing.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="why-choose-us">
+        <div class="why-choose-heading">
+            <h2>Why Choose Us</h2>
+        </div>
+        <div class="why-choose-content">
+            <div class="why-choose-one one">
+                <h3>Reliability</h3>
+                <p>Our experience and working made us a reliable company in the Moving industry. Many customers have already trusted us with our most valuable and sensitive possessions.</p>
+            </div>
+            <div class="why-choose-one two"><h3>Experience</h3>
+            <p>With years of working in this industry, we have discovered and perfected almost every skill that is important for moving your belongings.</p>
+        </div>
+            <div class="why-choose-one three"><h3>Custom Approach</h3>
+        <p>We understand everyone is different along with their needs. Our experts are here to help and provide you with the best solutions that fit your requirements well.</p></div>
+            <div class="why-choose-one four"><h3>Affordability</h3><p> We do not compromise the level of service with cost. We try to provide you best service with transparent and competitive pricing.</p></div>
+
+
+        </div>
+    </div>
+    <div class="about-contact-row">
+        <div class="about-contact-col">
+            <div class="about-contact-img">
+                <img src="./assets/images/5138237.jpg" width="100%" height="100%" alt="">
+            </div>
+        </div>
+        <div class="about-contact-col">
+            <div class="about-contact-detail">
+                <h2>Contact Us</h2>
+                <p>Are you ready to make your moving easy and fast . Contact Anytime movers experts by mailing or contacting at 984-235-6077 for free but accurate consultancy by our expert. Go to our Cost calculator section to discover quotes about your requirements. </p>
+                <p>
+                    With Anytime Movers you will get stress-free, efficient and reliable moving of your belongings.</p>
+                <a href="tel: 984-235-6077"><button>Call Now</button></a>
+            </div>
+        </div>
+    </div>
     <?php include './footer.php' ?>
-    
+
 </body>
+
 </html>
